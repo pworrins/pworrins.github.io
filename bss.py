5d72885ad27b839faf2dfe5b518014e446bfb6d8
Fitri Salwa <159430413+pworrins@users.noreply.github.com>
2024-04-15 21:36:34 +0700

    Update bss.py

--- bss.py
+++ bss.py
@@ -1,5 +1,5 @@
-import requests # type: ignore
-from bs4 import BeautifulSoup # type: ignore
+import requests 
+from bs4 import BeautifulSoup 
 import json
 from datetime import datetime
 
@@ -16,7 +16,6 @@ def scrape_website(url):
         if category_element and title_element and date_element:
             category = category_element.text.strip()
             title = title_element.text.strip()
-<<<<<<< HEAD
             
             # Periksa apakah ada teks pada elemen tanggal
             if date_element.text.strip():
@@ -27,11 +26,9 @@ def scrape_website(url):
             # Ambil link dari judul
             link = item.find('a')['href']
             
-=======
             publish_time = date_element.text.strip().split('-')[-1].strip()
             # Ambil link dari judul
             link = item.find('a')['href']
->>>>>>> 3b9dd31908d8bf034242bbc670fc011c909e2bcc
             articles.append({
                 'judul': title,
                 'kategori': category,
