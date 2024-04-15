d8b7ddbb0a362b75605427ceea1a15f015e1f0c7
Fitri Salwa <ftrslwa8@gmail.com>
2024-04-15 21:02:20 +0700

    update beritaa

--- js/table.js
+++ js/table.js
@@ -1,29 +1,13 @@
-function createTable(data) {
-    let table = '<table>';
-    table += '<thead><tr><th>No</th><th>Judul</th><th>Kategori</th><th>Waktu Rilis</th><th>Waktu Scraping</th></tr></thead>';
-    table += '<tbody>';
-    let i = 0;
-    data.forEach(item => {
-      table += '<tr>';
-      table += '<td>' + ++i + '</td>';
-      table += '<td>' + '<a href="' + item.url + '">' + item.judul + '</a>' + '</td>';
-      table += '<td>' + item.kategori + '</td>';
-      table += '<td>' + item['waktu publish'] + '</td>';
-      table += '<td>' + item['waktu scrape'] + '</td>';
-      table += '</tr>';
-    });
-    table += '</tbody></table>';
-    return table;
-  }
-  
-  $(document).ready(function() {
-    $.getJSON('republika_scraped_data.json', function(data) {
-      console.log(data);
-      const table = createTable(data);
-      $('#table-container').append(table);
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-      console.log('Error: ' + textStatus + ' - ' + errorThrown);
-    });
+$(document).ready(function(){
+  $.getJSON("republika_scraped_data.json", function(data){
+      $.each(data, function(index, item){
+          var row = "<tr>";
+          row += "<td>" + item.judul + "</td>";
+          row += "<td>" + item.kategori + "</td>";
+          row += "<td>" + item.waktu_publish + "</td>";
+          row += "<td>" + item.waktu_scraping + "</td>";
+          row += "</tr>";
+          $("#jsonTable tbody").append(row);
+      });
   });
-  
+});
